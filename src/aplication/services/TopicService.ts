36e1e433a014cd7b49bd6fac8e252d1da8a08033
jgilyam <jgilyam@folcode.com>
2023-03-25 16:24:48 -0300

    cambio de nombre

--- src/aplication/services/TopicService.ts
+++ src/aplication/services/TopicService.ts
@@ -1,9 +1,10 @@
+import { TopicOutDTO } from "../../domain/dtos";
 import { ITopicRepository } from "../../domain/interfaces/";
 
 export class TopicService {
   constructor(private readonly topicRepository: ITopicRepository) {}
 
-  public async findAllTopics() {
-    return this.topicRepository.findAll();
+  public async findAllTopics(name?: string): Promise<TopicOutDTO[]> {
+    throw new Error("metodo no implementado");
   }
 }
