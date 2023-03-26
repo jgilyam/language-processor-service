b17f14a9d3080f8568b2cdad015f8a0a2cf970b1
jgilyam <jgilyam@folcode.com>
2023-03-26 00:26:33 -0300

    generando contexxto de la api

---
+++ src/infra/db/repositories/index.ts
@@ -0,0 +1,4 @@
+export * from "./CampaingAxisRepositoryMock";
+export * from "./LanguageModelRepositoryMock";
+export * from "./MessageProcessedRepositoryMock";
+export * from "./TopicRepositoryMock";
--- src/infra/routes/containers/MessageProcessedContainer.ts
+++ src/infra/routes/containers/MessageProcessedContainer.ts
@@ -1,15 +1,40 @@
 import { CampaingAxisService, LanguageModelService, MessageProcessedSercice, TopicService } from "../../../aplication/services";
-import { MessageProcessedMapper } from "../../../domain/mappers";
+import { LanguageModelMapper, MessageProcessedMapper, TopicMapper } from "../../../domain/mappers";
+import { TextProcessorOpenAI } from "../../clients/TextProcessorOpenAI";
 import { MessageProcessedControler } from "../../controllers/MessageProcessedControler";
+import { CampaingAxisRepositoryMock } from "../../db/repositories/CampaingAxisRepositoryMock";
+import { LanguageModelRepositoryMock } from "../../db/repositories/LanguageModelRepositoryMock";
+import { MessageProcessedRepositoryMock } from "../../db/repositories/MessageProcessedRepositoryMock";
+import { TopicRepositoryMock } from "../../db/repositories/TopicRepositoryMock";
 
-const messageProcessedRepository = new IMessageProcessedRepository();
-const textProcessor = new ITextProcessor();
-const topicService = new TopicService();
-const languageModelService = new LanguageModelService();
-const campaignAxisService = new CampaingAxisService();
-const messageProcessedMapper = new MessageProcessedMapper();
+//repositories
+const messageProcessedRepository = new MessageProcessedRepositoryMock();
+const campaingAxisRepositoryMock = new CampaingAxisRepositoryMock();
+const languageModelRepositoryMock = new LanguageModelRepositoryMock();
+const topicRepositoryMock = new TopicRepositoryMock();
 
-const messageProcessedSercice = new MessageProcessedSercice();
+//mappers
+const languageModelMapper = new LanguageModelMapper();
+const topicMapper = new TopicMapper();
+const messageProcessedMapper = new MessageProcessedMapper(topicMapper, languageModelMapper);
+
+//clients
+const textProcessor = new TextProcessorOpenAI();
+
+//servicios
+const topicService = new TopicService(topicRepositoryMock);
+const languageModelService = new LanguageModelService(languageModelRepositoryMock);
+const campaignAxisService = new CampaingAxisService(campaingAxisRepositoryMock);
+const messageProcessedSercice = new MessageProcessedSercice(
+  messageProcessedRepository,
+  textProcessor,
+  topicService,
+  languageModelService,
+  campaignAxisService,
+  messageProcessedMapper
+);
+
+//controllers
 const messageProcessedController = new MessageProcessedControler(messageProcessedSercice);
 
 const context = {
