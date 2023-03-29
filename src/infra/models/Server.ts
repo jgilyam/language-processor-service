c09117ecb1fa813c9bb6b0b5e6c70a7cf36442c8
jgilyam <jgilyam@folcode.com>
2023-03-29 10:04:33 -0300

    cambiando variable port

--- src/infra/models/Server.ts
+++ src/infra/models/Server.ts
@@ -17,7 +17,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.SERVER_PORT || "8000";
+    this.port = process.env.PORT || "8000";
 
     this.dbConnection();
     this.middlewares();
